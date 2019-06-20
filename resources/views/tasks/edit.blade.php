b64893e46abf0cfbda1fac555b332a173eca9f6b
techgitek <techgitek@gmail.com>
2019-06-20 04:00:50 +0000

    index pass

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-<h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
+<h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
     <div class="row">
         <div class="col-6">
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,9 +15,6 @@
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
-                    <td>{{ $task->content }}</td>
-                    
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
